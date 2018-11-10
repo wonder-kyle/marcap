a0bab5cb214260e271047a3f5a8df26965092e50
Kyle Hong <wonderkyle@naver.com>
2018-11-10 02:17:52 +0000

    add low_memory func in marcap_date_range def

--- marcap_utils.py
+++ marcap_utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # marcap_utils.py - 시가총액 데이터를 위한 유틸함수
+#1995-05-02 ~ 2018-10-31 시가총액 데이터
 
 from datetime import datetime
 import numpy as np
@@ -29,12 +30,14 @@ def marcap_date(date):
     result.reset_index(drop=True, inplace=True)
     return result
 
-def marcap_date_range(start, end, code=None):
+def marcap_date_range(start, end, code=None, low_memory=False, chunksize=10**5):
     '''
     지정한 기간 데이터 가져오기
     :param datetime start: 시작일
     :param datetime end: 종료일
-    :param str code: 종목코드 (지정하지 않으면 모든 종목)
+    :param (str|list|set) code: 종목코드 혹은 종목코드 리스트 (지정하지 않으면 모든 종목)
+    :param low_memory: chunk로 나누어 road 여부 (메모리 절약)
+    :param chunksize: 하나의 chunk 단위 결정
     :return: DataFrame
     '''
     start = pd.to_datetime(start)
@@ -43,14 +46,48 @@ def marcap_date_range(start, end, code=None):
     for year in range(start.year, end.year + 1):
         try:
             csv_file = 'marcap/data/marcap-%s.csv.gz' % (year)
-            df = pd.read_csv(csv_file, dtype={'Code':str}, parse_dates=['Date'])
+            if low_memory:
+                chunks = pd.read_csv(csv_file, chunksize=chunksize, dtype={'Code':str}, parse_dates=['Date'])
+                df = pd.concat(_chunk_codefilter(chunks, code))
+            else:
+                df = pd.read_csv(csv_file, dtype={'Code':str}, parse_dates=['Date'])
+                if code:
+                    if type(code) in [list, set]:
+                        df = df[df['Code'].isin(code)]
+                    elif type(code)==str: 
+                        df = df[code == df['Code']]
+                    else: pass
             df_list.append(df)
         except Exception as e:
+            print('Error')
             pass
     df_merged = pd.concat(df_list)
     df_merged = df_merged[(start <= df_merged['Date']) & (df_merged['Date'] <= end)]  
     df_merged = df_merged.sort_values(['Date','Rank'])
-    if code:
-      df_merged = df_merged[code == df_merged['Code']]  
     df_merged.reset_index(drop=True, inplace=True)
-    return df_merged
+    return df_merged.rename(columns={'ChagesRatio':'ChangesRatio'})
+
+def _chunk_codefilter(chunks, code):
+    '''
+    pandas.read_csv 중 chunk를 code에 따라 필터링
+    :param pandas.io.parsers.TextFileReader chunks: chunks
+    :param (str|list|set) code: 종목코드 혹은 종목코드 리스트 (지정하지 않으면 모든 종목)
+    :yield: chunks의 단위 chunk
+    '''
+    if type(code) in [list, set]:
+        for chunk in chunks:
+            mask = chunk.iloc[:,0].isin(code)
+            if mask.all():
+                yield chunk
+            else:
+                yield chunk.loc[mask]
+    elif type(code)==str:
+        for chunk in chunks:
+            mask = chunk.iloc[:,0]==code
+            if mask.all():
+                yield chunk
+            else:
+                yield chunk.loc[mask]
+    else:
+        for chunk in chunks:
+            yield chunk
